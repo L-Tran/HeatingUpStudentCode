31adbb4f69c15dec0145ba21026f9e19f2360103
L-Tran <logan.tran@menloschool.org>
2026-01-18 14:15:21 -0800

    Finial commit

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * The class WeatherPatterns finds the longest span of days in which
  * each day’s temperature is higher than on the previous day in that sequence.
@@ -7,34 +9,53 @@
  */
 
 public class WeatherPatterns {
-
-
     /**
      * Longest Warming Trend
      * @param temperatures
      * @return the longest run of days with increasing temperatures
      */
+
+    public static ArrayList<Integer>[] list;
+    public static int[] paths;
+
     public static int longestWarmingTrend(int[] temperatures) {
-        // TODO: Write your code here!
-        int n = temperatures.length;
-        if(n == 0) {
-            return 0;
-        }
-        int[] lengths = new int[n];
-
-        for(int i = 0; i < n; i++) {
-            lengths[i] = 1;
-        }
-
-        int counter = 1;
-        // test if each length is the highest seen yet
-        for(int i = 0; i < n; i++) {
-
+        // Initialize array
+        list = new ArrayList[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            list[i] = new ArrayList<Integer>();
+            // Add each possible path from a temp to it's arraylist
+            for(int j = 0; j < i; j++) {
+                if(temperatures[j] < temperatures[i]) {
+                    list[i].add(j);
+                }
+            }
         }
 
+        // Initialize paths to store length before reaching each temp
+        paths = new int[temperatures.length];
 
+        // Find maxLength using recursion
+        int maxLength = 0;
+        for(int i = 0; i < temperatures.length; i++) {
+            maxLength = Math.max(maxLength, LongestPathTo(i, 0));
+        }
 
+        return maxLength;
+    }
 
-        return counter;
+    public static int LongestPathTo(int index, int len) {
+        // Use paths to shorten recursion
+        if(paths[index] != 0) {
+            return paths[index];
+        }
+
+        // Add up path length for each temp
+        for (int i = 0; i < list[index].size(); i++) {
+            len = Math.max(len, LongestPathTo(list[index].get(i), len));
+        }
+
+        // Set path length
+        paths[index] = len + 1;
+        return paths[index];
     }
 }
